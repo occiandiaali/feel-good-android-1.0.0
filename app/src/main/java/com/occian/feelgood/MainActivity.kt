cd1cce7c692c1d9b0cfc4ab29d1492725c74ebcd
occiandiaali <ocean@localhost.localdomain>
2020-06-07 18:08:51 +0100

    failed to successfuly set up volume seeckbar control and added extra quotes to string array

--- app/src/main/java/com/occian/feelgood/MainActivity.kt
+++ app/src/main/java/com/occian/feelgood/MainActivity.kt
@@ -18,13 +18,21 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private lateinit var mTTS: TextToSpeech
     private val strings = arrayOf(
-        "Look before you leap",
-        "A good name is worth more than silver or gold",
-        "A stitch in time saves nine",
-        "Many are mad few are roaming",
-        "Reach for the skies",
-        "You can do it"
+        "You don't have to be great to start. But you have to start to be great. Zig Ziglar",
+        "We may encounter many defeats but we must not be defeated. Maya Angelou",
+        "Walt Disney said. The way to get started is to quit talking and get doing",
+        "The pessimist sees difficulty in every opportunity. The optimist sees the opportunity in every difficulty. Winston Churchill",
+        "Will Rogers said. Don't let yesterday take up too much of today",
+        "It's not whether you get knocked down it's whether you get up. Vince Lombardi",
+        "Rob Siltanen said. People who are crazy enough to think they can change the world are the ones who do",
+        "Henry Ford said. Whether you think you can, or think you cannot. You're right.",
+        "The only limit to our realization of tomorrow is our doubts of today. Franklin D Roosevelt",
+        "Robert Schuller said. Todays accomplishments were yesterdays impossibilities",
+        "You are never too old to set another goal. Or to dream a new dream. C S Lewis",
+        "John Wooden said. Things work out best for those who make the best of how things work out"
     )
+   // private lateinit var strings: Array<String>
+
     private lateinit var animOut: Animation
     private lateinit var animIn: Animation
 
@@ -32,25 +40,25 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     var randomiser: Random = Random()
 
-    // variables for seekbar controls
+    // variables for pitch and pace controls
     var pitch: Float = 1.0f
     var pace: Float = 1.0f
-    var vol: Float = 1.0f
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val stringsTxt = applicationContext.assets.open("textfile.txt").bufferedReader().use {
+//            it.readText()
+//        }
+//        strings = arrayOf(stringsTxt)
 
         mTTS = TextToSpeech(this, this)
         animIn = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom_in)
         animOut = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom_out)
 
         am = getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        var maxVolume: Int = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
-        var currentVolume: Int = am.getStreamVolume(AudioManager.STREAM_MUSIC)
 
-        //var volumeBar = findViewById<SeekBar>(R.id.volBar)
         // see: https://www.youtube.com/watch?v=KAx5OAld8Hg
 
         // fab to launch spoken words
@@ -58,8 +66,6 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 spokenWords()
         } // fab listener
 
-        // controls seekbars
-        //pitchBar.setOnSeekBarChangeListener(seekBarListener)
 
     } // on create
 
@@ -70,15 +76,21 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         // listener to monitor when speech happens
         val speechListener = object : UtteranceProgressListener() {
             override fun onDone(utteranceId: String?) {
+                // fab icon changes to play when speech ends
                 floatingActionButton.setImageResource(android.R.drawable.ic_media_play)
             }
 
             override fun onError(utteranceId: String?) {}
 
             override fun onStart(utteranceId: String?) {
+                // animation rolls and fab icon changes to pause when speech begins
                 speakerImageView.startAnimation(animOut)
                 speakerImageView.startAnimation(animIn)
                 floatingActionButton.setImageResource(android.R.drawable.ic_media_pause)
+            }
+
+            override fun onStop(utteranceId: String?, interrupted: Boolean) {
+                super.onStop(utteranceId, interrupted)
             }
         } // speech listener set onutteranceprogresslistener
         mTTS.setOnUtteranceProgressListener(speechListener)
@@ -88,35 +100,28 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 pitch = progress / (pitchBar.max / 2).toFloat()
             }
-
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-        } // set on seekbar change
+        } // set on pitchbar change
 
         // listener to monitor when pace changes
         val paceBarListener = object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 pace = progress / (paceBar.max / 2).toFloat()
             }
-
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-        } // set on seekbar change
-
+        } // set on pacebar change
 
         var volListener = object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                am.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
-
+                am.setStreamVolume(AudioManager.STREAM_VOICE_CALL, progress, 0)
             }
-
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-        }
+        } // vol listener
 
+        // set all 3 callbacks
         pitchBar.setOnSeekBarChangeListener(pitchBarListener)
         paceBar.setOnSeekBarChangeListener(paceBarListener)
         volBar.setOnSeekBarChangeListener(volListener)
@@ -132,8 +137,6 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private fun spokenWords() {
         mTTS.setPitch(pitch)
         mTTS.setSpeechRate(pace)
-
-
         var randS = strings.get(randomiser.nextInt(strings.size))
         mTTS.speak(randS, TextToSpeech.QUEUE_FLUSH, null, TextToSpeech.ACTION_TTS_QUEUE_PROCESSING_COMPLETED)
     } // spoken words
